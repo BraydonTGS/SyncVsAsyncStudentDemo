abafa1f8aa76ef515141418be99a5bd48dbe15bb
Braydon Sutherland <BraydonSutherland@gmail.com>
2023-08-26 09:26:26 -0500

    Initial Create Async vs Sync Student Demo

---
+++ AsyncVsSync/Models/Async.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System.Threading.Tasks;
+
+namespace AsyncVsSync.Models
+{
+    public class Async
+    {
+        // Asynchronous Programming:
+        // In asynchronous programming, tasks can be started and allowed to run independently.The program doesn't wait for each task to finish before moving on.
+        // It's like ordering food online – you can continue doing other things while waiting for your order to arrive.
+        // Well-suited for tasks that involve waiting for external resources(like file I/O, network requests) or parallel processing.
+
+        // Pros of Asynchronous Programming:
+        // Improved performance as tasks can run concurrently, utilizing system resources effectively.
+        // Better responsiveness in applications, as the program doesn't get blocked by slow operations.
+        // Well-suited for scenarios where multiple tasks need to be handled without waiting for each other.
+
+        // Cons of Asynchronous Programming:
+        // More complex to understand due to asynchronous callbacks or handling.
+
+        // What is a Task //
+        // In C# asynchronous programming, a Task represents a unit of work that can be executed asynchronously.
+        // It's like a container for a piece of code that might take some time to complete, allowing your program to continue with other tasks in the meantime.
+        // It is a way to manage and control operations that can happen in the background without blocking your program's execution.
+        // It enables your program to be more responsive and efficient, especially when dealing with time-consuming or resource-intensive operations.
+
+        // Awaiting a Task: When you want to wait for a task to complete before continuing,
+        // you use the await keyword.This tells your program to pause at that point until the task finishes its work.
+        // This is how you can seamlessly mix asynchronous and synchronous code.
+    }
+}
---
+++ AsyncVsSync/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+
+Console.WriteLine("Starting synchronous image download...");
+Stopwatch syncStopwatch = new Stopwatch();
+syncStopwatch.Start();
+SynchronousImageDownload();
+syncStopwatch.Stop();
+Console.WriteLine($"Synchronous download took: {syncStopwatch.ElapsedMilliseconds} ms");
+
+Thread.Sleep(2000);
+
+Console.WriteLine("\nStarting asynchronous image download...");
+Stopwatch asyncStopwatch = new Stopwatch();
+asyncStopwatch.Start();
+await AsynchronousImageDownload();
+asyncStopwatch.Stop();
+Console.WriteLine($"Asynchronous download took: {asyncStopwatch.ElapsedMilliseconds} ms");
+
+
+static void SynchronousImageDownload()
+{
+    for (int i = 1; i <= 3; i++)
+    {
+        DownloadImageSync(i);
+    }
+}
+
+static async Task AsynchronousImageDownload()
+{
+    Task task1 = DownloadImageAsync(1);
+    Task task2 = DownloadImageAsync(2);
+    Task task3 = DownloadImageAsync(3);
+
+    await Task.WhenAll(task1, task2, task3);
+}
+
+static void DownloadImageSync(int imageNumber)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        string imageUrl = $"https://archives.bulbagarden.net/media/upload/3/3f/0143Snorlax.png";
+        byte[] imageBytes = client.GetByteArrayAsync(imageUrl).Result;
+        Console.WriteLine($"Downloaded image {imageNumber} synchronously.");
+    }
+}
+
+static async Task DownloadImageAsync(int imageNumber)
+{
+    using (HttpClient client = new HttpClient())
+    {
+        string imageUrl = $"https://archives.bulbagarden.net/media/upload/3/3f/0143Snorlax.png";
+        byte[] imageBytes = await client.GetByteArrayAsync(imageUrl);
+        Console.WriteLine($"Downloaded image {imageNumber} asynchronously.");
+    }
+}
+
+
+
+
+
+
